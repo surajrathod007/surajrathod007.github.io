42324129dea33c07981f5bdca65e9a5e9679d186
SurajSinh Rathod <surajsinh.rathod@sculptsoft.com>
2025-07-26 15:01:01 +0530

    feat: ready for prod

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2141953201"
+    serviceWorkerVersion: "408223186"
   }
 });
